06e19c31274a2f0aa33948aa9480ffab7a5633d4
Patrick Glavin <patjglavin@gmail.com>
2018-03-29 19:36:15 -0400

    stub files

---
+++ src/pages/favorites/favorites.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'basic-profile',
+    templateUrl: 'basicprofile.html'
+})
+export class BasicProfile {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
