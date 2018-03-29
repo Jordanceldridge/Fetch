b3764088478df77d0531dc805e684460115280a8
Patrick Glavin <patjglavin@gmail.com>
2018-03-29 19:33:37 -0400

    first for favorites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BasicProfile } from '../pages/BasicProfile/basicprofile';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    BasicProfile
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    BasicProfile
   ],
   providers: [
     StatusBar,
