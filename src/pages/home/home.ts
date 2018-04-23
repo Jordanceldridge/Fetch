d193fe1f9bf9966e7bbcefa1a1d2ab7d41de7bb1
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-04-23 15:10:39 -0400

    changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BasicProfilePage } from '../basic-profile/basic-profile';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,13 +9,25 @@ import { BasicProfilePage } from '../basic-profile/basic-profile';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  signinform: FormGroup;
+
 
   constructor(public navCtrl: NavController) {
+    this.signinform = new FormGroup({
+      city: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ] *')]),
+
+    })
+    
 
   }
+  
+
+
+
 
   goToOtherPage(){
     this.navCtrl.push(BasicProfilePage);
+
   }
 
 }
